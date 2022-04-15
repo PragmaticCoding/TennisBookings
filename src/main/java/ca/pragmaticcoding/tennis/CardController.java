b3ebd60aeeb38d3398a6bb4a987cd3ce579f29c1
Dave Barrett <barrettda@gmail.com>
2022-04-15 16:55:53 -0400

    Cleaned up some debugging output.

--- src/main/java/ca/pragmaticcoding/tennis/CardController.java
+++ src/main/java/ca/pragmaticcoding/tennis/CardController.java
@@ -4,16 +4,13 @@ import javafx.scene.layout.Region;
 
 public class CardController {
 
-    private final CardModel model;
     private final CardViewBuilder viewBuilder;
 
     public CardController(CardModel model) {
-        this.model = model;
         viewBuilder = new CardViewBuilder(model);
     }
 
     public Region getView() {
-        System.out.println("At builder");
         return viewBuilder.build();
     }
 }
--- src/main/java/ca/pragmaticcoding/tennis/MainViewBuilder.java
+++ src/main/java/ca/pragmaticcoding/tennis/MainViewBuilder.java
@@ -6,7 +6,6 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
 import javafx.util.Builder;
 
-import java.util.List;
 import java.util.Objects;
 
 public class MainViewBuilder implements Builder<Region> {
@@ -23,15 +22,12 @@ public class MainViewBuilder implements Builder<Region> {
         results.setPadding(new Insets(6));
         results.getStylesheets().add(Objects.requireNonNull(MainViewBuilder.class.getResource("/css/widgetsfx.css")).toString());
         results.getStylesheets().add(Objects.requireNonNull(MainViewBuilder.class.getResource("/css/tennis.css")).toString());
-        results.setMinSize(800, 600);
+        results.setMinSize(450, 340);
         results.setHgap(10);
         results.setVgap(10);
         model.getCardModelList().addListener((InvalidationListener) observable -> {
             results.getChildren().clear();
-            System.out.println(model.getCardModelList());
-            List<Region> cards = model.getCardModelList().stream().map(cardModel -> new CardController(cardModel).getView()).toList();
-            System.out.println(cards);
-            results.getChildren().addAll(cards);
+            results.getChildren().addAll(model.getCardModelList().stream().map(cardModel -> new CardController(cardModel).getView()).toList());
         });
         return results;
     }
